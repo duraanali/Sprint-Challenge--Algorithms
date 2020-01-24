6bc92e94d7287645b770cf93911b6059d6fd56be
Duraan Ali <ali@sahalsolutions.com>
2020-01-24 12:04:29 -0600

    count done

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -6,5 +6,15 @@ Your function must utilize recursion. It cannot contain any loops.
 
 
 def count_th(word):
+    str2 = 'th'
+    n1 = len(word)
+    n2 = len(str2)
 
-    # TBC
+    # Base Case
+    if (n1 == 0 or n1 < n2):
+        return 0
+
+    if (word[0: n2] == str2):
+        return 1 + count_th(word[n2 - 1:])
+
+    return count_th(word[n2 - 1:])
